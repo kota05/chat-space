e239b3710234a93be1fc2df30cc4f2c010042b65
kota05 <k.kake.ilinks@gmail.com>
2020-07-21 11:32:21 +0900

    deploy-chat-space11

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.6.5です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/chat-space01.pem'] 
+set :ssh_options, auth_methods: ['publickey'],keys: ['~/.ssh/chatspace01.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
